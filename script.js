e230d3ce5b4c703a272f5a9f58d56f382b9f3282
Majd <95650197+majd-ibrahem@users.noreply.github.com>
2022-05-06 17:43:45 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+
+const sliders = document.querySelectorAll('.slide');
+const auto = true;
+
+
+const nextSlide = () => {
+    // Get current class
+    const current = document.querySelector('.current');
+    // Remove current class
+    current.classList.remove('current');
+    // Check if there is a sibling element
+    if(current.nextElementSibling)
+    {
+        current.nextElementSibling.classList.add('current');
+    }
+    else
+    {
+        //Add current to start
+        sliders[0].classList.add('current');
+    }
+}
+
+ setInterval(nextSlide,20000);
